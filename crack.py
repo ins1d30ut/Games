4636dd5bf5901c649d8b89564a1e87b10c41ebf7
ins1d30ut <nfteebsv@protonmail.com>
2023-01-11 17:01:39 -0500

    commit update prompts

--- crack.py
+++ crack.py
@@ -5,3 +5,14 @@ if age > 18:
   print("\nYou're old enough to smoke crack!")
 else:
   print("\nYou'll be able to smoke crack one day!")
+
+prompt = "\nIf you can't smoke crack yet, which drug would you like to do instead? "
+prompt += "\n(Enter 'quit' when you are finished.) "
+
+while True:
+  drug = input(prompt)
+
+  if drug == 'quit':
+    break
+  else:
+    print(f"You really want to do that {drug.title()}!?")
